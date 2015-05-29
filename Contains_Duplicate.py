cea0ba9e3ce04540827c9676e75848df59d2400f
Ke <kesang0156357@gmail.com>
2015-05-29 10:43:36 -0400

    add comments and general stategy for contain_duplicate

--- Contains_Duplicate.py
+++ Contains_Duplicate.py
@@ -2,6 +2,12 @@
 Given an array of integers, find if the array contains any duplicates. 
 Your function should return true if any value appears at least twice in the array, 
 and it should return false if every element is distinct.
+1)the method implemented in more python
+two general stategy:
+2)use a set, and check the set with each element, hash it, if element already in the set, return True
+the time-complex is O(n), the space-complex is O(n)
+3)sort the array, if the next to each other are equal, return True
+the time-complex is O(n*log(n)), the space complex is O(1)
 '''
 class Solution:
 		# @param {integer[]} nums
