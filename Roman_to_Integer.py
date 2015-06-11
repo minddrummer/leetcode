bf7e89a3d0b9c38f77ee966e1f8dd5caff113538
Ke <kesang0156357@gmail.com>
2015-06-11 11:24:47 -0400

    add unfinished roman_to_integer and start to code for the reverse_bit.py

---
+++ Roman_to_Integer.py
@@ -0,0 +1,19 @@
+# Given a roman numeral, convert it to an integer.
+# Input is guaranteed to be within the range from 1 to 3999.
+
+
+
+class Solution:
+    # @param {string} s
+    # @return {integer}
+    def romanToInt(self, s):
+    	'''
+    	see the following java result; boring coding here
+    	'''
+
+
+
+
+if __name__ == '__main__':
+	test  = Solution()
+	test.romanToInt()        
