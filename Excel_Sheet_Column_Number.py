1b634dcce8e494589066d791094ef8fbc14fe92c
Ke <kesang0156357@gmail.com>
2015-06-18 12:39:20 -0400

    excel_sheet_column_number

--- Excel_Sheet_Column_Number.py
+++ Excel_Sheet_Column_Number.py
@@ -13,16 +13,21 @@
 #     AB -> 28 
 
 
-
-
-
 class Solution:
     # @param {string} s
     # @return {integer}
     def titleToNumber(self, s):
-        
-
+    	s = s.lower()
+    	letter_dict = dict(zip([chr(one) for one in range(97,123)], range(1,27)))
+        #print letter_dict
+        s_len = len(s)
+        total = 0
+        for i in range(s_len):
+        	#print letter_dict[s[i]]
+        	#print 26**(s_len-i-1)
+        	total += letter_dict[s[i]]*26**(s_len-i-1)
+        return total
 
 if __name__ == '__main__':
 	test = Solution()
-	test.titleToNumber()
+	print test.titleToNumber('aa')
