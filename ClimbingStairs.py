5d2fad5f4ccf2a4b76c0a0844b34c7a6d3226b1a
Ke <kesang0156357@gmail.com>
2015-07-19 23:05:20 -0400

    finish the naive way for the climbing stairs problem. but not effcient

--- ClimbingStairs.py
+++ ClimbingStairs.py
@@ -1,6 +1,8 @@
 # You are climbing a stair case. It takes n steps to reach to the top.
 
 # Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+# this is a dynamic programming program
+
 
 class Solution:
     # @param {integer} n
@@ -10,11 +12,12 @@ class Solution:
     		return 1
     	if n == 1:
     		return 1
-    	2 :11 2 
-    	3: 111 12 21 
-    	4: 1111 121 211 112 22 
-    	5: 11111 221 
-    	10: 22111111
+    	return self.climbStairs(n-1) + self.climbStairs(n-2)
+    	# 2 :11 2 
+    	# 3: 111 12 21 
+    	# 4: 1111 121 211 112 22 
+    	# 5: 11111 221 
+    	# 10: 22111111
 
 
 
@@ -22,7 +25,9 @@ class Solution:
 
 if __name__ == '__main__':
 	test = Solution()
-	print test.climbStairs(1)
-	print test.climbStairs(2)
-	print test.climbStairs(3)
+	print test.climbStairs(0) #0
+	print test.climbStairs(1) #1
+	print test.climbStairs(2) #2
+	print test.climbStairs(3) #3
+	print test.climbStairs(4) #5
 
