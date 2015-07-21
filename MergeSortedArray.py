ba2955c82944e524075bd11050e4184f32bfb1f4
Ke <kesang0156357@gmail.com>
2015-07-21 12:20:19 -0400

    mergesortedarray, strange question, raw method

---
+++ MergeSortedArray.py
@@ -0,0 +1,69 @@
+# Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+# Note:
+# You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+
+
+
+
+
+
+class Solution:
+    # @param {integer[]} nums1
+    # @param {integer} m
+    # @param {integer[]} nums2
+    # @param {integer} n
+    # @return {void} Do not return anything, modify nums1 in-place instead.
+    def merge(self, nums1, m, nums2, n):
+    	#n1c = 0
+    	if n == 0: return nums1
+    	
+    	size_1 = m
+    	j = 0
+    	for n2 in nums2:
+    		
+    		i = 0
+    		ending = True
+    		while i < size_1:
+    			if nums1[i] > n2:
+    				tmp = nums1[i:]
+    				nums1[i] = n2
+    				nums1[(i+1):] = tmp
+    				size_1 = len(nums1)
+    				ending = False
+    				break
+    			i += 1
+    		if ending == True:
+    			for item in nums2[j:]:
+    				nums1.append(item)
+    			break
+    		j += 1
+
+    	return nums1	
+class Solution:
+    # @param A  a list of integers
+    # @param m  an integer, length of A
+    # @param B  a list of integers
+    # @param n  an integer, length of B
+    # @return nothing
+    def merge(self, A, m, B, n):
+        tmp = [0 for i in range(m + n)]
+        i = 0; j = 0; k = 0
+        while i < m and j < n:
+            if A[i] <= B[j]:
+                tmp[k] = A[i]; i += 1
+            else:
+                tmp[k] = B[j]; j += 1
+            k += 1
+        if i == m:
+            while k < m + n:
+                tmp[k] = B[j]; k += 1; j += 1
+        else:
+            while k < m + n:
+                tmp[k] = A[i]; i += 1; k += 1
+        for i in range(0, m + n):
+            A[i] = tmp[i]
+
+if __name__ == '__main__':
+	test = Solution()
+	print test.merge([1,2,3],3,[3,4],2)    	
