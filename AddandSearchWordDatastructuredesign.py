cfb8a26d9adc6dd22be8b65831db4b475e6ea022
Ke <kesang0156357@gmail.com>
2015-07-27 11:28:17 -0400

    addandSarchWordDataStructure

--- AddandSearchWordDatastructuredesign.py
+++ AddandSearchWordDatastructuredesign.py
@@ -20,22 +20,51 @@
 
 
 class WordDictionary:
-    # initialize your data structure here.
-    def __init__(self):
-        
+	# initialize your data structure here.
+	def __init__(self):
+		
 
-    # @param {string} word
-    # @return {void}
-    # Adds a word into the data structure.
-    def addWord(self, word):
-        
+	# @param {string} word
+	# @return {void}
+	# Adds a word into the data structure.
+	def addWord(self, word):
+		
 
-    # @param {string} word
-    # @return {boolean}
-    # Returns if the word is in the data structure. A word could
-    # contain the dot character '.' to represent any one letter.
-    def search(self, word):
-        
+	# @param {string} word
+	# @return {boolean}
+	# Returns if the word is in the data structure. A word could
+	# contain the dot character '.' to represent any one letter.
+	def search(self, word):
+		if '.' not in word:
+			if word in self.val:
+				return True
+			else:
+				return False
+		else:
+			for i in range(len(word)):
+				if word[i] != '.':
+					if '.' not in word[i+1:]:
+						if word in self.val:
+							return True
+						else:
+							return False
+					else:
+						continue
+				else:
+					new_val = []
+					for item in self.val:
+						if len(item)-1 >= i:
+							item = list(item)
+							item[i] = '.'
+							new_val.append(''.join(item))
+						else:
+							new_val.append(item)
+					self.val = new_val        
+			if word in self.val:
+				return True
+			else:
+				return False
+		
 
 # Your WordDictionary object will be instantiated and called as such:
 # wordDictionary = WordDictionary()
